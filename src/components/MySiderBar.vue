7ecab5b9e53be37e5824d7188151b8a89881aaec
luoxa <973434397@qq.com>
2019-04-14 14:16:00 +0800

    style： change style of 'MySider'

--- src/components/MySiderBar.vue
+++ src/components/MySiderBar.vue
@@ -29,10 +29,10 @@
 <style scoped lang="less">
   .my-silder-navbar{position:fixed;left:0px;top:40px;bottom:0px;width:70px;background:#0A1733;}
   ul.my-nav{margin:0;padding:0;}
-  ul.my-nav>li{list-style:none;color:#ffffff99;margin:40px 0;text-align:center;font-size:12px;line-height:24px;}
+  ul.my-nav>li{list-style:none;color:#ffffff99;margin:50px 0;text-align:center;font-size:12px;line-height:12px;}
   ul.my-nav>li.active{color:#fff;border-left:3px solid #fff;}
   ul.my-nav>li:hover{color:#fff;}
-  ul.my-nav>li>i{width:100%;font-size:20px;}
+  ul.my-nav>li>i{width:100%;font-size:20px;margin-bottom: 16px;}
   ul.my-tool{margin:0;padding:0;border-top:1px solid rgba(255,255,255,.2);}
   ul.my-tool>li{list-style:none;color:#ffffff;margin:40px 0;text-align:center;font-size:12px;line-height:24px;}
   ul.my-tool>li>i{width:100%;font-size:30px;}
